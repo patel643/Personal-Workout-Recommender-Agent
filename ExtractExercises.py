61e2d1b0770b18a8e656affc8f44a8a731849f7f
Cade Zacharias <zacha085@umn.edu>
2018-03-23 10:00:26 -0500

    Create ExtractExercises.py

---
+++ ExtractExercises.py
@@ -0,0 +1,30 @@
+import codecs
+import os
+
+
+Exercises = {}
+path = "WorkoutsTXT/(00)finished"
+for filename in os.listdir(path):
+    
+    file = path + "/" + filename #.replace(" ","\ ")
+
+    Workout = codecs.open(file,encoding='utf-8',mode='r').read()
+    Workout = Workout.replace('}','')
+    ID = Workout.split('\n')[0]
+    Circuits = Workout.split('{')
+    for circuit in Circuits:
+        items = circuit.split('\n')
+        reps = items[0]
+        for index in range(1,len(items)-1):
+            temp = items[index].split(' ')
+            if len(temp) > 1:
+                reps = temp[0]
+                exercise = ' '.join(temp[1:])
+                exercise = exercise.lower()
+                if exercise in Exercises:
+                    Exercises[exercise].append(reps)
+                else:
+                    Exercises[exercise] = [reps]
+
+            else:
+                continue

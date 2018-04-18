cfb0facabb7eceb9210d685832586243f3af218d
Cade Zacharias <zacha085@umn.edu>
2018-04-18 16:20:11 -0500

    Code Update

--- Probability.py
+++ Probability.py
@@ -3,18 +3,41 @@ import os
 import numpy as np
 from ExtractExercises import *
 
+
+
+def normalize(p):
+    p = p/np.sum(p)
+##    dif = 1.0 - np.sum(p)
+##    p[0] += dif
+    return p
+
+
+def Generate_Circuit(num_exercises, ExList,dist, factor):
+    circuit = []
+    for i in range(num_exercises):
+        #Select Random exercise based on distribution
+        dist = normalize(dist)
+        choice = np.random.choice(len(UniDist),1, p = dist)[0]
+        circuit.append(ExList[choice])
+        
+        #affect distribution based on selected exercises
+        dist[choice] *= factor
+ #       dist = normalize(dist)
+        
+    return circuit
+
+
+
 Exercises = ExtractExercises()
 
 ExerciseSet = sorted(list(Exercises.keys()))
 
 UniDist = np.full(len(ExerciseSet),1/(len(ExerciseSet)))
 
-choice = np.random.choice(len(UniDist),1, p = UniDist)[0]
+#UniDist = normalize(UniDist)
+#choice = np.random.choice(len(UniDist),1, p = UniDist)[0]
+for i in range(10):
+    ckt = Generate_Circuit(100,ExerciseSet,UniDist, i)
+    print(len(set(ckt))/len(ckt))
+    print(len(set(ckt))/len(ckt))
 
-for i,ex in enumerate(Exercises):
-    if "push" in ex:
-        UniDist[i]*=5
-
-def normalize(p):
-    p = p/np.sum(p)
-    return p
